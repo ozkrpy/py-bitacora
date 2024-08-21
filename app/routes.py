430b88a6b312b6d46967e53b848fce04451b3fd0
ozkrpy@gmail.com <ozkrpy@gmail.com>
2024-08-21 18:56:07 -0400

    202407151555

--- app/routes.py
+++ app/routes.py
@@ -256,7 +256,7 @@ def nueva_operacion(tarjeta):
                             operacion=False,
                             pagado=True,
                             id_agrupador_gastos=3)
-            db.session.add(carga) 
+            db.session.add(carga)
             db.session.commit()
         flash('Nueva operacion agregada con exito.')
         return redirect(url_for('movimientos_mes', mes=mes))
